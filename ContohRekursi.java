ac3f2ded98c1d36f686d9aeb77f847560ec8c04e
Jeheskiel Kaja <2381052@unai.edu>
2024-09-20 05:37:04 +0700

    handson4

---
+++ ContohRekursi.java
@@ -0,0 +1,5 @@
+public class ContohRekursi {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ControlReturnValue.java
@@ -0,0 +1,12 @@
+public class ControlReturnValue {
+    public static void sebutkanNama() {
+        System.out.println("Nama saya Jeheskiel Kaja");
+    }
+    public static int kembalikanAngka() {
+        int hasilPerkalian = 60;
+        return hasilPerkalian;
+    }
+    public static void main(String[] args) {
+
+    }
+}
